2182153cd9f42e82b799d565abcaddf96fca5fd9
Txory92 <txoryarroba@gmail.com>
2026-08-12 15:49:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => {
+    console.log("Service Worker: Instal·lat");
+});
+
+self.addEventListener("activate", () => {
+    console.log("Service Worker: Activat");
+});
